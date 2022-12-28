3c4b6228d58233f3fc5a736241bf93bb65b500f6
Joseph Eng <joseng2358@gmail.com>
2022-12-28 13:16:02 -0800

    Main layout fixes Some starter code for main activity Remove unwanted horizontal biases Remove content description on layout Change name to text input type Suppress text and image contrast warnings Reformat

--- app/src/main/java/com/robototes/scouting/scoutingapp/MainActivity.java
+++ app/src/main/java/com/robototes/scouting/scoutingapp/MainActivity.java
@@ -2,13 +2,32 @@ package com.robototes.scouting.scoutingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static <T> T getRandFromArray(T[] arr) {
+        int index = (int) (arr.length * Math.random());
+        return arr[index];
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        TextView splashText = (TextView) findViewById(R.id.splash_view);
+        String[] splashes = res.getStringArray(R.array.splashes);
+        splashText.setText(getRandFromArray(splashes));
+
+        Button startButton = (Button) findViewById(R.id.start_button);
+
+        EditText matchNumber = (EditText) findViewById(R.id.match_number);
+        EditText teamNumber = (EditText) findViewById(R.id.team_number);
+        EditText name = (EditText) findViewById(R.id.name);
     }
 }
